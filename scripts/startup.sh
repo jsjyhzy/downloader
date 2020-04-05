98f5557febc6c1084b4a2ec92e37a274a33e5b29
hzy <i@huzheyang.com>
2020-04-05 14:28:07 +0800

    fix parallel not startup 3 scripts

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -4,4 +4,4 @@ SPATH=/downloader/scripts
 
 mkdir -p $CONF_PATH
 
-ls $SPATH | grep -v startup.sh | parallel --tag --line-buffer --workdir $SPATH bash {}
+ls $SPATH | grep -v startup.sh | parallel -j3 --tag --line-buffer --workdir $SPATH bash {}
